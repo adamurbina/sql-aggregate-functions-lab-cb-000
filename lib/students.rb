8e0c2e42dbd4e7bc2e2e1e264594b20f60bb773f
Adam Urbina <adamurbina@gmail.com>
2018-07-19 14:22:33 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,7 +1,7 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
-    command = "SELECT MAX(gpa) FROM students;"
+    command = SELECT MAX(gpa) FROM students;
     @sql_runner.execute_sql(command)
 end
 
